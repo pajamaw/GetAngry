cb525f6892870ffde3059b19528874d6836ce727
pajamaw <pjpeterww@gmail.com>
2016-10-20 22:08:47 -0400

    fixing env front end variables with conditionals to get to heroku round 8

--- app/assets/javascripts/ang/services/BillService.js
+++ app/assets/javascripts/ang/services/BillService.js
@@ -4,6 +4,3 @@ app.factory('bill', function($resource, BillEnv){
 });
 //###google doesn't have great info on their civics api column
 //##going to use legiscan api because it's more up to date an dbetter data
-
-//##curl "https://www.googleapis.com/civicinfo/v2/voterinfo?key=<YOUR_API_KEY>&address=1263%20Pacific%20Ave.%20Kansas%20City%20KS&electionId=2000"
-//
